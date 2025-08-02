03bee2c9b2f81f3125938fa3ffc30bee543443fc
Yogesh chavan <chavanyogesh8600@gmail.com>
2025-08-02 11:44:47 +0530

    Add login and log user with auth middleware

--- src/controllers/User.controller.js
+++ src/controllers/User.controller.js
@@ -7,8 +7,6 @@ import ApiResponce from "../utils/apiResponce.js";
 export const registerUser = asyncHandler(async (req, res) => {
 
   const { username, password, email, fullname } = req.body;
-
-
   console.log("Form Fields:", req.body);
   console.log("Uploaded Files:", req.files);
 
@@ -28,11 +26,6 @@ export const registerUser = asyncHandler(async (req, res) => {
   if (!avtarLocalPath) {
    throw new ApiError(400, "Avatar is required");
   }
-
-  
-
-  
-
   // Upload to Cloudinary
   const avatarResponse = await uploadOnCloudinary(avtarLocalPath);
   const coverImageResponse = coverImageLocalPath
@@ -67,3 +60,75 @@ export const registerUser = asyncHandler(async (req, res) => {
     .status(201)
     .json(new ApiResponce(201, "User registered successfully", createdNewUser));
 });
+
+export const loginUser =asyncHandler(async (req,res)=>{
+
+  const {username , email , password ,fullname} = req.body;
+
+  if(!username || !email){
+    throw new ApiError(400 , "user name or email is required")
+  }
+
+  const userOccured= await User.find({
+    $or:[{username} , {email}]
+  })
+
+  if(!userOccured){
+    throw new ApiError(400 , "User not found")
+  }
+
+  const isPasswordValid = await userOccured.isPasswordCorrect(password)
+
+  if(!isPasswordValid){
+    throw new ApiError(400 , "Invalid credentials")
+  }
+
+  const {accessToken}= await genereteAccessToken(userOccured._id , email , password ,fullname)
+  const {refreshToken} = await genereteRefreshToken(userOccured._id)
+
+  const loggedInUser = await User.findById(userOccured._id).select("-password refreshToken");
+
+  const options={
+    httpOnly : true,
+    secure : true,
+  }
+
+  return res.status(200)
+  .cookie("accessToken",accessToken,options)
+  .cookie("accessToken",refreshToken,options)
+  .json(
+    new ApiResponce(
+      200,
+      {
+        userOccured :loggedInUser , accessToken , refreshToken
+      },
+      "user logged in successfuly"
+    )
+  )
+})
+
+export const logoutUser = asyncHandler(async (req,res)=>{
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set:{
+       accessToken: undefined
+      }
+    },
+    {
+      new:true
+    }
+  )
+
+   const options={
+    httpOnly : true,
+    secure : true,
+  }
+
+  return res.status(200)
+  .clearCookies("accessToken",options)
+  .clearCookies("refreshToken",options)
+  .json(new ApiResponce(200 , "User logged out"))
+})
+
+
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,32 @@
+import { header } from "express-validator";
+import asyncHandler from "../utils/asyncHandler";
+import ApiResponce from "../utils/apiResponce";
+import jwt from "jsonwebtoken"
+import User from "../models/user.models";
+import ApiError from "../utils/apiError";
+
+
+export const verifyJWT= asyncHandler(async (req , res , next)=>{
+
+    try {
+        const token = req.cookies?.accessToken || header("authorization")?.replace("Bearer ", "");
+    
+        if(!token){
+            throw new ApiResponce(401 , "Unauthorized request")
+        }
+    
+        const decodedToken = jwt.verify(token , process.env.ACCESS_TOKEN_SECRETE)
+    
+        const user = await User.findById(decodedToken._id).select("-password -refreshToken");
+    
+        if(!user){
+            throw new ApiError(401 , "invalid access token ")
+        }
+    
+        req.user=user;
+    
+        next()
+    } catch (error) {
+        throw new ApiError(401 , error?.message || "Invalid access token")
+    }
+})
